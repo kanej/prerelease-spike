770eaeb2925afcdfb96f63b7495244b3bb796654
John Kane <john@kanej.me>
2024-06-21 16:02:25 +0100

    Feat/deep dep (#7)
    * minor tweak to d
    
    * add a changeset

---
+++ .changeset/blue-meals-think.md
@@ -0,0 +1,5 @@
+---
+"@ignored/prerelease-spike-d": patch
+---
+
+Just a d change
--- v-next/d/src/index.js
+++ v-next/d/src/index.js
@@ -4,5 +4,6 @@ export default function d() {
   const localRequire = createRequire(import.meta.url);
   const info = localRequire("../package.json");
 
+  // and again
   console.log("from d with version <" + info.version + ">");
 }
