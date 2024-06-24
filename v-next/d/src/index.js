1583949acbb9190d56d9bf93842dd61db3f2f918
John Kane <john@kanej.me>
2024-06-24 16:39:19 +0100

    feat: small addition to d (#11)

--- v-next/d/src/index.js
+++ v-next/d/src/index.js
@@ -4,6 +4,6 @@ export default function d() {
   const localRequire = createRequire(import.meta.url);
   const info = localRequire("../package.json");
 
-  // and again and again
+  // and again and again and again
   console.log("from d with version <" + info.version + ">");
 }
